a528eed8e91af52414a9edfc170c43e54cba7097
AnyMagicalChickenWillDo <30678191+JadLevesque@users.noreply.github.com>
2022-03-02 03:15:17 -0500

    Important changes
    Examples changed to use the last revision of grail_pp.h

--- main.c
+++ main.c
@@ -3,10 +3,19 @@
 #include "grail_pp.h"
 
 
-#define PLUS_1(x...) 1 +
+#define PLUS(x,y) x + y
+#define PAIR(rec,x) {x, rec}
 
-#define PLUS_SEQ_SIZE(s...) GRAIL_PP_SEQ_FOREACH(,PLUS_1,s)
+GRAIL_PP_FOLD_ARGS (PAIR, 0,1,2,3,4,5)
+GRAIL_PP_FOLD_ARGS (PLUS, 0,1,2,3,4,5)
 
-int main () {
-    printf ("%i", PLUS_SEQ_SIZE(()()()()()()()()()()()()()()()) 1);
-}
+#define INC(x) x + 1
+
+
+GRAIL_PP_SCAN (GRAIL_PP_FOLD_ARGS GRAIL_PP_EMPTY(PAIR, GRAIL_PP_MAP_ARGS (INC,1,2,3,4,5))))
+
+#define foo \
+GRAIL_PP_REVIVE(GRAIL_PP_SEQ_FOREACH) \
+GRAIL_PP_SEQ_FOREACH (,INC, (1)(2)(3))
+
+GRAIL_PP_SCAN (GRAIL_PP_SEQ_FOREACH(, GRAIL_PP_STR, (foo) (2) (3)))
