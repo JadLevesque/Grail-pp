541c70c019739cd966f3f3a56d3bd1cabc488116
AnyMagicalChickenWillDo <30678191+JadLevesque@users.noreply.github.com>
2022-03-02 04:18:57 -0500

    Added GRAIL_PP_UNTIL example
    Added an example using GRAIL_PP_UNTIL to generate the Fibonacci sequence

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 #include "grail_pp.h"
 
-
+#if 0
 #define PLUS(x,y) x + y
 #define PAIR(rec,x) {x, rec}
 
@@ -19,3 +19,24 @@ GRAIL_PP_REVIVE(GRAIL_PP_SEQ_FOREACH) \
 GRAIL_PP_SEQ_FOREACH (,INC, (1)(2)(3))
 
 GRAIL_PP_SCAN (GRAIL_PP_SEQ_FOREACH(, GRAIL_PP_STR, (foo) (2) (3)))
+
+  
+#define FIB(n...) GRAIL_PP_SCAN (GRAIL_PP_UNTIL (FIB_PRED, FIB_STEP, FIB_OP, 0, 1, n))
+
+#define FIB_OP(a,b,n...) a,
+
+#define FIB_PRED(a,b,n...) GRAIL_PP_IS_EMPTY_VA_ARGS(n)
+
+#define FIB_STEP(a,b,x,v...) b,(a+b),v
+
+#else
+
+int main () {
+    int a[] = {FIB(,,,,,,,,,)};
+    int i;
+
+    for (i = 0; i < sizeof a / sizeof a[0]; i++) {
+        printf ("%i\n", a [i]);
+    }
+}
+#endif
